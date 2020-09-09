16db77c2458ca06534c3a596f4d87b830ecd2461
Vlad Matviyuk <jr.matviyuk@gmail.com>
2020-09-09 23:58:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Main from "./components/Main";
+import CardsList from "./components/CardsList";
 
-function App() {
+const App = () => {
+  const [items, setItem] = React.useState([
+    { id: 1, image: "koko.jpg", title: "Item 1", status: false },
+    { id: 2, image: "koko.jpg", title: "Item 2", status: false },
+    { id: 3, image: "koko.jpg", title: "Item 3", status: false },
+    { id: 4, image: "koko.jpg", title: "Item 4", status: false },
+    { id: 5, image: "koko.jpg", title: "Item 5", status: false },
+    { id: 6, image: "koko.jpg", title: "Item 6", status: false },
+    { id: 7, image: "koko.jpg", title: "Item 6", status: false },
+    { id: 8, image: "koko.jpg", title: "Item 6", status: false },
+    { id: 9, image: "koko.jpg", title: "Item 6", status: false },
+    { id: 10, image: "koko.jpg", title: "Item 10", status: false },
+  ]);
+
+  function testYourLuck() {
+    let randomNumber = Math.floor(Math.random() * 10);
+
+    let newArray = [...items];
+    newArray[randomNumber] = {
+      ...newArray[randomNumber],
+      status: !newArray[randomNumber].status,
+    };
+
+    setItem(newArray);
+    console.log(randomNumber);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main testClick={testYourLuck} />
+      <CardsList items={items} />
     </div>
   );
-}
+};
 
 export default App;
